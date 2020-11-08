f7203a28afc06dbe1ab284e2f270377af3b01a70
miShelbyT <mi.shelbyrose@gmail.com>
2020-11-08 11:17:11 -0500

    easy breezy done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // write your code below!
+
+function happyHolidays () {
+	return `Happy holidays!`
+}
+
+function happyHolidaysTo (name) {
+	return `Happy holidays, ${name}!`
+}
+
+function happyCustomHolidayTo (holiday, name) {
+	return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown (holiday, days) {
+	return `It's ${days} days until ${holiday}!`
+}
+
+console.log(happyHolidays("shelby"))
+
+console.log(happyCustomHolidayTo("sandwich day", "shelby"))
+
+console.log(holidayCountdown("sandwich day", 6))
